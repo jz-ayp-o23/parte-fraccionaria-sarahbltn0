2b38001ee9656ca6539b906b763b103eb2e56bbd
sarahbltn <sarah.beltran@iteso.mx>
2023-10-10 19:38:50 -0600

    Agregar numeros negativos

--- parte_fracc.py
+++ parte_fracc.py
@@ -6,9 +6,10 @@ Inserta el encabezado aquí y escribe tu código abajo
 numero = float(input("Introduzca un número: "))
 
 # Proceso
-if numero - int(numero) > 0:
+if numero - int(numero) > 0 or int(numero) < 0:
     salida = "Sí tiene decimales"
 elif numero - int(numero) == 0:
     salida = "No tiene decimales"
 
-print(salida)
+# Salida
+print(salida)   
